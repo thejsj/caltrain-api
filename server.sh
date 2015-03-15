8f9984ccbc7fe441e352cead474f01eb9521b37e
thejsj <jorge.silva@thejsj.com>
2015-03-14 20:20:22 -0700

    Update init

--- server.sh
+++ server.sh
@@ -1,9 +1,11 @@
-echo 'python.sh'
+echo 'python'
 cd /app
 pip install -r requirements.pip
 python scraper -i -g -c
-echo 'server.sh'
+echo 'server'
 cd /app/
 npm install -g babel
 npm install
+echo 'Installed!'
+npm list
 npm start
