e4c3722e1b5198ab961cd19a867c79a86a927c2c
thejsj <jorge.silva@thejsj.com>
2015-03-15 19:18:55 -0700

    Update server.sh

--- server.sh
+++ server.sh
@@ -2,11 +2,11 @@ echo 'python'
 cd /app
 pip install -q -r requirements.pip
 python scraper -i -g -c
-mkdocs build
 echo 'server'
 cd /app/
 rm -rf node_modules
 npm cache clean
 npm install
 echo 'Installed!'
+npm run docs
 npm start
