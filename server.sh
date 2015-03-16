3516a0098cf9e276cd553621cecb09878881a07d
thejsj <jorge.silva@thejsj.com>
2015-03-15 19:17:52 -0700

    Change docs to docco

--- server.sh
+++ server.sh
@@ -1,6 +1,6 @@
 echo 'python'
 cd /app
-pip install -r requirements.pip
+pip install -q -r requirements.pip
 python scraper -i -g -c
 mkdocs build
 echo 'server'
@@ -9,5 +9,4 @@ rm -rf node_modules
 npm cache clean
 npm install
 echo 'Installed!'
-npm list
 npm start
